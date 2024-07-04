e141aa15275e87f2137b149c11daf9e56ca48b45
ncip48 <mbahcip00@gmail.com>
2024-07-04 18:27:03 +0700

    rev: slip gaji

--- resources/views/laporan/absen.blade.php
+++ resources/views/laporan/absen.blade.php
@@ -3,95 +3,91 @@
 @section('title', 'Absen')
 
 @section('content')
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Absen</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item ">
-                                <a href="#">Home</a>
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="d-flex align-items-center">
+            <div class="me-auto">
+                <h4 class="page-title">Laporan Absen</h4>
+                <div class="d-inline-block align-items-center">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('dashboard') }}"><i class="mdi mdi-home-outline"></i></a>
                             </li>
                             <li class="breadcrumb-item">
-                                <a href="#">Laporan</a>
+                                <a href="#">Laporan</i></a>
                             </li>
-                            <li class="breadcrumb-item active">
-                                Laporan Absen
-                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">Laporan Absen</li>
                         </ol>
-                    </div>
+                    </nav>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="d-flex flex-column">
-                                        <h3 class="card-title mt-1">
-                                            Laporan Absen
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                Filter laporan absen karyawan
-                                <div class="d-flex justify-content-between align-items-center mt-2">
-                                    <div class="col-12">
-                                        {{-- <li><a class="dropdown-item" href="#">Action</a></li> --}}
-                                        <form method="POST" action="{{ route('laporan.absen.print') }}">
-                                            <div class="row mb-1">
-                                                @csrf
-                                                <div class="form-group col-4 d-flex flex-row align-items-center">
-                                                    <label class="mb-0 mr-2" for="">Bulan</label>
-                                                    <select class="form-control month-filter" name="bulan">
-                                                        @foreach (['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'] as $bulan)
-                                                            <option value="{{ $loop->index + 1 }}"
-                                                                {{ $loop->index == date('n') - 1 ? 'selected' : '' }}>
-                                                                {{ $bulan }}</option>
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-                                                <div class="form-group col-4 d-flex flex-row align-items-center">
-                                                    <label class="mb-0 mr-2" for="">Tahun</label>
-                                                    <select class="form-control year-filter" name="tahun">
-                                                        @for ($tahun = date('Y') - 5; $tahun <= date('Y'); $tahun++)
-                                                            <option value="{{ $tahun }}"
-                                                                {{ $tahun == date('Y') ? 'selected' : '' }}>
-                                                                {{ $tahun }}</option>
-                                                        @endfor
-                                                    </select>
-                                                </div>
-                                                <div class="form-group col-4 d-flex flex-row align-items-center">
-                                                    <button type="submit" class="btn btn-primary">
-                                                        Cetak Laporan Absen</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
-                        <!-- /.card -->
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <!-- /.row -->
             </div>
-            <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
+        </div>
     </div>
-    <!-- /.content-wrapper -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="d-flex flex-column">
+                                    <h3 class="card-title mt-1">
+                                        Laporan Absen
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            Filter laporan absen karyawan
+                            <div class="d-flex justify-content-between align-items-center mt-2">
+                                <div class="col-12">
+                                    {{-- <li><a class="dropdown-item" href="#">Action</a></li> --}}
+                                    <form method="POST" action="{{ route('laporan.absen.print') }}">
+                                        <div class="row mb-1">
+                                            @csrf
+                                            <div class="form-group col-4 d-flex flex-row align-items-center">
+                                                <label class="mb-0 me-10" for="">Bulan</label>
+                                                <select class="form-control month-filter" name="bulan">
+                                                    @foreach (['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'] as $bulan)
+                                                        <option value="{{ $loop->index + 1 }}"
+                                                            {{ $loop->index == date('n') - 1 ? 'selected' : '' }}>
+                                                            {{ $bulan }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-4 d-flex flex-row align-items-center">
+                                                <label class="mb-0 me-10" for="">Tahun</label>
+                                                <select class="form-control year-filter" name="tahun">
+                                                    @for ($tahun = date('Y') - 5; $tahun <= date('Y'); $tahun++)
+                                                        <option value="{{ $tahun }}"
+                                                            {{ $tahun == date('Y') ? 'selected' : '' }}>
+                                                            {{ $tahun }}</option>
+                                                    @endfor
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-4 d-flex flex-row align-items-center">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Cetak Laporan Absen</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 @endsection
