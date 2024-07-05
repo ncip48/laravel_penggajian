de14857150f0fe51c0f6af0cb54a88a80913c5d1
ncip48 <mbahcip00@gmail.com>
2024-07-05 18:47:19 +0700

    rev: crud lembur

--- resources/views/lembur/action.blade.php
+++ resources/views/lembur/action.blade.php
@@ -2,92 +2,52 @@
 $is_edit = isset($data);
 ?>
 
-@extends('layouts.app')
-
-@section('title', $is_edit ? 'Edit Lembur' : 'Tambah Lembur')
-
-@section('content')
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>{{ $is_edit ? 'Edit' : 'Tambah' }} Lembur</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item ">
-                                <a href="#">Home</a>
-                            </li>
-                            <li class="breadcrumb-item">
-                                <a href="#">Transaksi</a>
-                            </li>
-                            <li class="breadcrumb-item active">
-                                {{ $is_edit ? 'Edit' : 'Tambah' }} Lembur
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
+<form id="main-form" class="form-horizontal"
+    action="{{ $is_edit ? route('lembur.update', $data) : route('lembur.store') }}" role="form" method="POST"
+    autocomplete="off" data-reload="true">
+    @csrf
+    {!! $is_edit ? method_field('PUT') : '' !!}
+    <div id="modal-master" class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">{{ $is_edit ? 'Edit' : 'Tambah' }} Lembur</h5>
+                <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">{{ $is_edit ? 'Edit' : 'Tambah' }} Lembur</h3>
-                            </div>
-                            <form id="main-form"
-                                action="{{ $is_edit ? route('lembur.update', $data) : route('lembur.store') }}"
-                                method="POST" autocomplete="off" data-back="{{ route('lembur.index') }}">
-                                @csrf
-                                {!! $is_edit ? method_field('PUT') : '' !!}
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <label for="id_karyawan">Karyawan</label>
-                                        <select class="custom-select form-control-sm" id="id_karyawan" name="id_karyawan"
-                                            @if (isset($data)) readonly @endif>
-                                            <option value="" selected disabled>Pilih Karyawan</option>
-                                            @foreach ($karyawans as $item)
-                                                <option value="{{ $item->id_karyawan }}"
-                                                    @if (isset($data->id_karyawan) && $item->id_karyawan == $data->id_karyawan) selected @endif>
-                                                    {{ $item->nama_karyawan }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="tanggal">Tanggal</label>
-                                        <input type="date" class="form-control" id="tanggal" name="tanggal"
-                                            value="{{ isset($data->tanggal) ? $data->tanggal : '' }}"
-                                            @if (isset($data)) readonly @endif>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="jam">Jumlah Jam</label>
-                                        <input type="text" class="form-control" id="jam" name="jam"
-                                            value="{{ isset($data->jam) ? $data->jam : '' }}">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="keterangan">Keterangan</label>
-                                        <input type="text" class="form-control" id="keterangan" name="keterangan"
-                                            value="{{ isset($data->keterangan) ? $data->keterangan : '' }}">
-                                    </div>
-                                </div>
-
-                                <div class="card-footer">
-                                    <button id="btn-save" type="submit" class="btn btn-primary">Simpan</button>
-                                </div>
-                            </form>
-                        </div>
+                    <div class="form-group required">
+                        <label for="id_karyawan">Karyawan</label>
+                        <select class="form-select form-control" id="id_karyawan" name="id_karyawan" required>
+                            <option value="" selected disabled>Pilih Karyawan</option>
+                            @foreach ($karyawans as $item)
+                                <option value="{{ $item->id_karyawan }}"
+                                    @if (isset($data->id_karyawan) && $item->id_karyawan == $data->id_karyawan) selected @endif>
+                                    {{ $item->nama_karyawan }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group required">
+                        <label for="tanggal">Tanggal</label>
+                        <input type="date" class="form-control" id="tanggal" name="tanggal"
+                            value="{{ isset($data->tanggal) ? $data->tanggal : '' }}" required>
+                    </div>
+                    <div class="form-group required">
+                        <label for="jam">Jumlah Jam</label>
+                        <input type="text" class="form-control" id="jam" name="jam"
+                            value="{{ isset($data->jam) ? $data->jam : '' }}" required>
+                    </div>
+                    <div class="form-group required">
+                        <label for="keterangan">Keterangan</label>
+                        <input type="text" class="form-control" id="keterangan" name="keterangan"
+                            value="{{ isset($data->keterangan) ? $data->keterangan : '' }}" required>
                     </div>
                 </div>
             </div>
-            <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
+            <div class="modal-footer modal-footer-uniform d-flex">
+                <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" id="btn-save"
+                    class="btn btn-sm btn-primary float-end">{{ $is_edit ? 'Update' : 'Simpan' }}</button>
+            </div>
+        </div>
     </div>
-    <!-- /.content-wrapper -->
-@endsection
+</form>
--- resources/views/lembur/index.blade.php
+++ resources/views/lembur/index.blade.php
@@ -3,110 +3,102 @@
 @section('title', 'Lembur')
 
 @section('content')
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Lembur</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item ">
-                                <a href="#">Home</a>
+    <div class="content-header">
+        <div class="d-flex align-items-center">
+            <div class="me-auto">
+                <h4 class="page-title">Lembur</h4>
+                <div class="d-inline-block align-items-center">
+                    <nav>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('dashboard') }}"><i class="mdi mdi-home-outline"></i></a>
                             </li>
                             <li class="breadcrumb-item">
-                                <a href="#">Transaksi</a>
+                                <a href="#">Transaksi</i></a>
                             </li>
-                            <li class="breadcrumb-item active">
-                                Data Lembur
-                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">Lembur</li>
                         </ol>
-                    </div>
+                    </nav>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="d-flex flex-column">
-                                        <h3 class="card-title mt-1">
-                                            Daftar Lembur
-                                        </h3>
-                                    </div>
-                                    <div class="card-tools">
-                                        <a class="btn btn-sm btn-primary mt-1" href="{{ route('lembur.create') }}"><i
-                                                class="fas fa-plus"></i>
-                                            Tambah Lembur</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                <div class="table-responsives">
-                                    <table class="table table-striped table-hover table-full-width" id="main_table">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Tanggal</th>
-                                                <th>NIK</th>
-                                                <th>Nama</th>
-                                                <th>Jam</th>
-                                                <th>Keterangan</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($lemburs as $index => $lembur)
-                                                <tr>
-                                                    <td>{{ $index + 1 }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($lembur->tanggal)->locale('id')->isoFormat('D MMMM Y') }}
-                                                    </td>
-                                                    <td>{{ $lembur->karyawan->nik }}</td>
-                                                    <td>{{ $lembur->karyawan->nama_karyawan }}</td>
-                                                    <td>{{ $lembur->jam }}</td>
-                                                    <td>{{ $lembur->keterangan ?? '-' }}</td>
-                                                    <td>
-                                                        <div class="d-flex">
-                                                            <a href="{{ route('lembur.edit', $lembur->id_lembur) }}"
-                                                                class="btn btn-sm btn-warning"><i
-                                                                    class="fas fa-edit"></i></a>
-                                                            <form data-reload="true" id="main-form"
-                                                                action="{{ route('lembur.destroy', $lembur) }}"
-                                                                method="POST" class="ml-1 delete-form">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button class="confirm-text btn btn-sm btn-danger">
-                                                                    <i class="fas fa-trash"></i>
-                                                                </button>
-                                                            </form>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                    {{-- {{ $asets->links('vendor.pagination.bootstrap-4') }} --}}
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
-                        <!-- /.card -->
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <!-- /.row -->
             </div>
-            <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
+        </div>
     </div>
-    <!-- /.content-wrapper -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="box">
+                    <div class="box-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex flex-column">
+                                <h4 class="box-title mt-1">
+                                    Lembur
+                                </h4>
+                            </div>
+                            <div class="box-tools">
+                                <a href="#" class="btn btn-sm btn-primary mt-1 ajax_modal"
+                                    data-url="{{ route('lembur.create') }}"><i class="fa fa-plus"></i>
+                                    Tambah</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="table-responsives">
+                            <table class="table table-striped table-hover table-full-width" id="main_table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Tanggal</th>
+                                        <th>NIK</th>
+                                        <th>Nama</th>
+                                        <th>Jam</th>
+                                        <th>Keterangan</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($lemburs as $index => $lembur)
+                                        <tr>
+                                            <td>{{ $index + 1 }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($lembur->tanggal)->locale('id')->isoFormat('D MMMM Y') }}
+                                            </td>
+                                            <td>{{ $lembur->karyawan->nik }}</td>
+                                            <td>{{ $lembur->karyawan->nama_karyawan }}</td>
+                                            <td>{{ $lembur->jam }}</td>
+                                            <td>{{ $lembur->keterangan ?? '-' }}</td>
+                                            <td>
+                                                <div class="d-flex">
+                                                    <a href="#"
+                                                        data-url="{{ route('lembur.edit', $lembur->id_lembur) }}"
+                                                        class="btn btn-sm btn-warning ajax_modal"><i
+                                                            class="fa fa-pencil"></i></a>
+                                                    <form data-reload="true" id="main-form-delete"
+                                                        action="{{ route('lembur.destroy', $lembur) }}" method="POST"
+                                                        class="ms-1 delete-form">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button class="confirm-text btn btn-sm btn-danger">
+                                                            <i class="fa fa-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            {{-- {{ $asets->links('vendor.pagination.bootstrap-4') }} --}}
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 @endsection
