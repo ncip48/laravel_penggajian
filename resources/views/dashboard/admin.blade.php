caecbe36837775750c9f69cf3aa1ffe65b5f5c62
ncip48 <mbahcip00@gmail.com>
2024-07-05 18:12:48 +0700

    rev: dashboard

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -23,63 +23,60 @@
 
     <!-- Main content -->
     <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-3 col-6">
+        <div class="row">
+            <div class="col-lg-3 col-6">
 
-                    <div class="small-box bg-info">
-                        <div class="inner">
-                            <h3>{{ $data_karyawan }}</h3>
-                            <p>Data Karyawan</p>
-                        </div>
-                        <div class="icon">
-                            {{-- <i class="nav-icon fas fa-user-alt"></i> --}}
-                        </div>
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{ $data_karyawan }}</h3>
+                        <p>Data Karyawan</p>
+                    </div>
+                    <div class="icon">
+                        {{-- <i class="nav-icon fas fa-user-alt"></i> --}}
                     </div>
                 </div>
-
-                <div class="col-lg-3 col-6">
-
-                    <div class="small-box bg-success">
-                        <div class="inner">
-                            <h3>{{ $data_admin }}</h3>
-                            <p>Data Admin</p>
-                        </div>
-                        <div class="icon">
-                            {{-- <i class="nav-icon fas fa-users"></i> --}}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-6">
-
-                    <div class="small-box bg-warning">
-                        <div class="inner">
-                            <h3>{{ $data_jabatan }}</h3>
-                            <p>Data Jabatan</p>
-                        </div>
-                        <div class="icon">
-                            {{-- <i class="nav-icon fas fa-graduation-cap"></i> --}}
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-6">
-
-                    <div class="small-box bg-danger">
-                        <div class="inner">
-                            <h3>{{ $data_kehadiran }}</h3>
-                            <p>Data Kehadiran</p>
-                        </div>
-                        <div class="icon">
-                            {{-- <i class="nav-icon fas fa-calendar"></i> --}}
-                        </div>
-                    </div>
-                </div>
-
             </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $data_admin }}</h3>
+                        <p>Data Admin</p>
+                    </div>
+                    <div class="icon">
+                        {{-- <i class="nav-icon fas fa-users"></i> --}}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $data_jabatan }}</h3>
+                        <p>Data Jabatan</p>
+                    </div>
+                    <div class="icon">
+                        {{-- <i class="nav-icon fas fa-graduation-cap"></i> --}}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $data_kehadiran }}</h3>
+                        <p>Data Kehadiran</p>
+                    </div>
+                    <div class="icon">
+                        {{-- <i class="nav-icon fas fa-calendar"></i> --}}
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
     </div>
